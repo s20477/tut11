7c3ef8d532f7f6715117242739f8db1e12fb44c6
s20477 <61776873+s20477@users.noreply.github.com>
2020-06-03 00:31:29 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,46 +1,24 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Item> items = new ArrayList<>();
-
-        items.add(new Item("s87483", "xyz1"));
-        items.add(new Item("s21438", "xyz2"));
-        items.add(new Item("s64738", "xyz3"));
-        items.add(new Item("s84375", "xyz4"));
-        items.add(new Item("s09358", "xyz6"));
-        items.add(new Item("s19358", "xyz7"));
-        items.add(new Item("s29358", "xyz8"));
-        items.add(new Item("s39328", "xyz9"));
-        items.add(new Item("s49358", "xyz10"));
-        items.add(new Item("s59358", "xyz11"));
-        items.add(new Item("s69308", "xyz12"));
-        items.add(new Item("s79358", "xyz13"));
-        items.add(new Item("s89378", "xyz14"));
-        items.add(new Item("s99358", "xyz15"));
-
-
-        for (Item i : items) {
-            i.show();
+    }
+    public  static int readNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Provide number: ");
+        if (num < 0) {
+            throw new NegativeArraySizeException();
         }
-
-        HashMap<String, String> itemsMap = new HashMap();
-        for (Item i : items) {
-            itemsMap.put(i.getID(), i.getName());
+        return num;
+    }
+    public static void fillArray(int[] arr) {
+        for (int i = 0; i<arr.length; i++) {
+            System.out.println("Negative number occured.");
+            arr[i] = 0;
         }
-
-        for (Map.Entry<String, String> entries : itemsMap.entrySet()) {
-            System.out.println(entries.getKey());
-            System.out.println(entries.getValue());
-        }
-        List<Item> items2 = items.subList(0, 4);
-
     }
 }
---
+++ NegativeNumberExeption.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class NegativeNumberExeption {
+    int[] arr = new int[10];
+
+    fillArray(arr);
+
+    System.out.println();
+    for (int i : arr) {
+        System.out.println(i + "");
+    }
+
+
+}
